aad7df64d88dd6717e2e804866ec4b4e27c45e91
bboyd146 <82745040+bboyd146@users.noreply.github.com>
2021-10-22 13:09:41 -0500

    queries and mutations set up for login/signup and user info

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -1,10 +1,42 @@
-// const { AuthenticationError } = require("apollo-server-errors");
-// const { User } = require("../models");
-// const { signToken } = require("../utils/auth");
+const { AuthenticationError } = require("apollo-server-errors");
+const { User } = require("../models");
+const { signToken } = require("../utils/auth");
 
-// const resolvers = {
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id }).select(
+                    "-__v -password"
+                );
+                return userData;
+            }
+            throw new AuthenticationError("Not logged in");
+        },
+    },
+    Mutation: {
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
 
+            if (!user) {
+                throw new AuthenticationError("Invalid credentials");
+            }
 
-// }
+            const correctPassword = await user.isCorrectPassword(password);
+            if (!correctPassword) {
+                throw new AuthenticationError("Invalid credentials");
+            }
+            const token = signToken(user);
 
-// module.exports = resolvers
+            return { token, user };
+        },
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+
+            return { token, user };
+        },
+    }
+}
+
+module.exports = resolvers
