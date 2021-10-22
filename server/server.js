eb45303adabe31cad4f96448376e679766a61c43
bboyd146 <82745040+bboyd146@users.noreply.github.com>
2021-10-22 12:21:47 -0500

    typeDefs and resolvers setup

--- server/server.js
+++ server/server.js
@@ -18,15 +18,7 @@ app.use(express.static("public"));
 //     graphiql: true 
 // }));
 
-// mongoose.connect(
-//     process.env.MONGODB_URI || 'mongodb://localhost/cosmic',
-//     {
-//         useNewUrlParser: true,
-//         useUnifiedTopology: true,
-//         useCreateIndex: true,
-//         useFindAndModify: false
-//     }
-// );
+
 
 // routes
 // app.use(require("./routes/api.js"));
