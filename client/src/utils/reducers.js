83d844f9541656274f2bd4b4a4e45aeafe6eee6b
Osvaldo Zavaleta <osvzavaleta@gmail.com>
2021-10-23 11:40:04 -0500

    client/src/utils/reducers.js updated: imported data from actions.js

---
+++ client/src/utils/reducers.js
@@ -0,0 +1,12 @@
+import { useReducer } from 'react';
+import {
+    UPDATE_PRODUCTS,
+    ADD_TO_CART,
+    ADD_MULTIPLE_TO_CART,
+    REMOVE_FROM_CART,
+    CLEAR_CART,
+    UPDATE_CART_QUANTITY,
+    TOGGLE_CART,
+    UPDATE_GENRES,
+    UPDATE_CURRENT_GENRE
+} from './actions';
