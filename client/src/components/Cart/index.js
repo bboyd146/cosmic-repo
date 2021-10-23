6b53ec4285e6a3ffc333ed027ad0fd3ef6972651
bboyd146 <82745040+bboyd146@users.noreply.github.com>
2021-10-23 11:31:25 -0500

    imported spripe api key

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -1,6 +1,7 @@
+import { loadStripe } from '@stripe/stripe-js';
 
 
-
+const stripePromise = loadStripe('sk_test_51JnmXxIaPmg7X2tXbhdOxIhaEw7z5QcM02U4oahA7RMUaV0VkSnuVGvqMShB2qhlALHJ0BViR9sLuWoSA9bQLUDd00lrBMOLyw');
 const Cart = () => {
     return (
         <div>
