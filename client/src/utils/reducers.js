6271d8647b77ba19250c9ef706bc7e11b22e9678
Osvaldo Zavaleta <osvzavaleta@gmail.com>
2021-10-23 11:46:14 -0500

    client/src/utils/reducers.js updated: exported cases for cart functionality

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,4 +1,4 @@
-import { useReducer } from 'react';
+import { useReducer } from "react";
 import {
     UPDATE_PRODUCTS,
     ADD_TO_CART,
@@ -8,5 +8,83 @@ import {
     UPDATE_CART_QUANTITY,
     TOGGLE_CART,
     UPDATE_GENRES,
-    UPDATE_CURRENT_GENRE
-} from './actions';
+    UPDATE_CURRENT_GENRE,
+} from "./actions";
+
+export const reducer = (state, action) => {
+    switch (action.type) {
+        case UPDATE_PRODUCTS:
+            return {
+                ...state,
+                products: [...action.products],
+            };
+
+        case ADD_TO_CART:
+            return {
+                ...state,
+                cartOpen: true,
+                cart: [...state.cart, action.product],
+            };
+
+        case ADD_MULTIPLE_TO_CART:
+            return {
+                ...state,
+                cart: [...state.cart, ...action.products],
+            };
+
+        case UPDATE_CART_QUANTITY:
+            return {
+                ...state,
+                cartOpen: true,
+                cart: state.cart.map((product) => {
+                    if (action._id === product._id) {
+                        product.purchaseQuantity = action.purchaseQuantity;
+                    }
+                    return product;
+                }),
+            };
+
+        case REMOVE_FROM_CART:
+            let newState = state.cart.filter((product) => {
+                return product._id !== action._id;
+            });
+
+            return {
+                ...state,
+                cartOpen: newState.length > 0,
+                cart: newState,
+            };
+
+        case CLEAR_CART:
+            return {
+                ...state,
+                cartOpen: false,
+                cart: [],
+            };
+
+        case TOGGLE_CART:
+            return {
+                ...state,
+                cartOpen: !state.cartOpen,
+            };
+
+        case UPDATE_GENRES:
+            return {
+                ...state,
+                genres: [...action.genres],
+            };
+
+        case UPDATE_CURRENT_GENRE:
+            return {
+                ...state,
+                currentGenre: action.currentGenre,
+            };
+
+        default:
+            return state;
+    }
+};
+
+export function useProductReducer(initialState) {
+    return useReducer(reducer, initialState);
+}
