fb502ebe02ebe3c94c2d84dd45d963a21af8e015
Josh Nolan <nolanjosh1979@gmail.com>
2021-10-25 16:47:26 -0500

    added page transition animations

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -28,7 +28,15 @@ export default function Header() {
         <div className="relative bg-cream overflow-hidden">
             <div className="max-w-7xl mx-auto">
                 <div className="relative z-10 pb-8 bg-cream sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
-                    
+                <svg
+            className="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-cream transform translate-x-1/2"
+            fill="currentColor"
+            viewBox="0 0 100 100"
+            preserveAspectRatio="none"
+            aria-hidden="true"
+          >
+            <polygon points="50,0 100,0 50,100 0,100" />
+          </svg>
                     <Popover>
                         <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
                             <nav className="relative flex items-center justify-between sm:h-10 lg:justify-start" aria-label="Global">
@@ -101,17 +109,19 @@ export default function Header() {
                     <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
                         <div className="sm:text-center lg:text-left">
                             
-                            <h1 className="text-4xl tracking-tight font-logo text-pink sm:text-5xl md:text-6xl">
+                            <h1 className="text-5xl tracking-tight font-logo text-pink sm:text-5xl md:text-8xl">
                                 <span className="block xl:inline ">Cosmic Records</span>{' '}
-                                <span className="block text-indigo-600 xl:inline">online record store</span>
                             </h1>
+                            <h2 className="text-5xl tracking-tight font-logo text-pink sm:text-5xl md:text-6xl">
+                            <span className="block text-indigo-600 xl:inline">online store</span>
+                            </h2>
                             <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                                 <div className="rounded-md shadow">
                                     <a
                                         href="/Inventory"
                                         className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-cream bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
                                     >
-                                        Shop
+                                        Shop!
                                     </a>
                                 </div>
                             </div>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,18 @@
 // import Logo from '../Logo'
+import { motion } from 'framer-motion'
 import Header from '../components/Header';
 
 
 
 const Home = () => {
     return (
-        <>
+        <motion.div 
+        exit={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        initial={{ opacity: 0 }}
+        >
         <Header />
-        </>
+        </motion.div>
     )
 }
 
