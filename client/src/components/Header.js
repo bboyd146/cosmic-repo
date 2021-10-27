d9cd9fb34ea3d08d9761b6cadb5f1a398dfedda4
bboyd146 <82745040+bboyd146@users.noreply.github.com>
2021-10-26 21:05:17 -0500

    updating navtabs

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -17,10 +17,9 @@ import { Popover, Transition } from '@headlessui/react'
 import { MenuIcon, XIcon } from '@heroicons/react/outline'
 
 const navigation = [
-    { name: 'Product', href: '#' },
-    { name: 'Features', href: '#' },
-    { name: 'Marketplace', href: '#' },
-    { name: 'Company', href: '#' },
+    { name: 'Home', href: '#' },
+    { name: 'Inventory', href: '#' },
+    { name: 'Cart', href: '#' },
 ]
 
 export default function Header() {
@@ -42,7 +41,7 @@ export default function Header() {
                             <nav className="relative flex items-center justify-between sm:h-10 lg:justify-start" aria-label="Global">
                                 <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
                                     <div className="flex items-center justify-between w-full md:w-auto">
-                                        <div className="-mr-2 flex items-center md:hidden">
+                                        <div className="-mr-2 flex items-center lg:hidden">
                                             <Popover.Button className="bg-cream rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
                                                 <span className="sr-only">Open main menu</span>
                                                 <MenuIcon className="h-6 w-6" aria-hidden="true" />
@@ -65,7 +64,7 @@ export default function Header() {
                         >
                             <Popover.Panel
                                 focus
-                                className="absolute z-10 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
+                                className="absolute z-10 top-0 inset-x-0 p-2 transition transform origin-top-right lg:hidden"
                             >
                                 <div className="rounded-lg shadow-md bg-cream ring-1 ring-black ring-opacity-5 overflow-hidden">
                                     <div className="px-5 pt-4 flex items-center justify-between">
--- client/src/components/NavTabs.js
+++ client/src/components/NavTabs.js
@@ -1,17 +1,18 @@
 import { Link } from 'react-router-dom';
 import { MdOutlineShoppingCart } from "react-icons/md";
 
+
 const NavTabs = () => {
     return (
-        <nav className="flex nav-tabs text-center">
-            <div className="flex flex-wrap">
-                <Link to="/">Home</Link>
-                <Link to="/Inventory">Inventory</Link>
-                <Link to="/About">About</Link>
-                <Link to="/Cart"><MdOutlineShoppingCart/></Link>
-            </div>
-        </nav>
-        
+
+                    <nav className="flex nav-tabs text-center hidden lg:block">
+                        <div className="flex items-end">
+                            <Link to="/" className="text-base font-small text-gray-900 hover:text-gray-700">Home</Link>
+                            <Link to="/Inventory" className="text-base font-small text-gray-900 hover:text-gray-700">Inventory</Link>
+                            <Link to="/About" className="text-base font-small text-gray-900 hover:text-gray-700">About</Link>
+                            <Link to="/Cart" className="text-base font-small text-gray-900 hover:text-gray-700"><MdOutlineShoppingCart /></Link>
+                        </div>
+                    </nav>
     )
 }
 
