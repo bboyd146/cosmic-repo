406c5cbc375a6e9eb2b64cb81c439751a08a5f66
Osvaldo Zavaleta <osvzavaleta@gmail.com>
2021-10-26 20:26:31 -0500

    client/src/components/NavTabs.js updated: Login and Signup links added

--- client/src/components/NavTabs.js
+++ client/src/components/NavTabs.js
@@ -7,6 +7,8 @@ const NavTabs = () => {
             <div className="flex flex-wrap">
                 <Link to="/">Home</Link>
                 <Link to="/Inventory">Inventory</Link>
+                <Link to="/Login">Login</Link>
+                <Link to="/Signup">Signup</Link>
                 <Link to="/Cart"><MdOutlineShoppingCart/></Link>
             </div>
         </nav>
