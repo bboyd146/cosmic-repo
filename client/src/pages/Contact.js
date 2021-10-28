136b150765ab8a4feb53c38d68955b8a66fdddee
Osvaldo Zavaleta <osvzavaleta@gmail.com>
2021-10-28 09:45:13 -0500

    client/src/pages/Contact.js updated: merged formed sections to create a single form

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,57 +1,15 @@
 export default function Contact() {
     return (
         <>
-            <div>
-                <div className="md:grid md:grid-cols-3 md:gap-6">
-                    <div className="md:col-span-1">
-                    </div>
-                    <div className="mt-5 md:mt-0 md:col-span-2">
-                        <form action="#" method="POST">
-                            <div className="shadow sm:rounded-md sm:overflow-hidden">
-                                <div className="px-4 py-5 bg-white space-y-6 sm:p-6">
-                                    <div>
-                                        <label
-                                            htmlFor="about"
-                                            className="block text-sm font-medium text-gray-700"
-                                        >
-                                            About
-                                        </label>
-                                        <div className="mt-1">
-                                            <textarea
-                                                id="about"
-                                                name="about"
-                                                rows={3}
-                                                className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md"
-                                                placeholder="Description"
-                                                defaultValue={""}
-                                            />
-                                        </div>
-                                        <p className="mt-2 text-sm text-gray-500">
-                                            Please provide a brief description of your problem, concern, or suggestion.
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-
-            <div className="hidden sm:block" aria-hidden="true">
-                <div className="py-5">
-                    <div className="border-t border-gray-200" />
-                </div>
-            </div>
-
             <div className="mt-10 sm:mt-0">
                 <div className="md:grid md:grid-cols-3 md:gap-6">
                     <div className="md:col-span-1">
                         <div className="px-4 sm:px-0">
                             <h3 className="text-lg font-medium leading-6 text-gray-900">
-                                Personal Information
+                                Reach Out!
                             </h3>
                             <p className="mt-1 text-sm text-gray-600">
-                                Use a permanent address where you can receive mail.
+                                Fill out the provided form to contact our support team
                             </p>
                         </div>
                     </div>
@@ -123,9 +81,28 @@ export default function Contact() {
                                             />
                                         </div>
 
-                                        
+                                        <div className="col-span-6 sm:col-span-6">
+                                        <label
+                                            htmlFor="about"
+                                            className="block text-sm font-medium text-gray-700"
+                                        >
+                                            About
+                                        </label>
+                                        <div className="mt-1">
+                                            <textarea
+                                                id="about"
+                                                name="about"
+                                                rows={3}
+                                                className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md"
+                                            />
+                                        </div>
+                                        <p className="mt-2 text-sm text-gray-500">
+                                            Please provide a brief description of your problem, concern, or suggestion.
+                                        </p>
+                                    </div>
                                     </div>
                                 </div>
+
                                 <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
                                     <button
                                         type="submit"
@@ -139,13 +116,6 @@ export default function Contact() {
                     </div>
                 </div>
             </div>
-
-            <div className="hidden sm:block" aria-hidden="true">
-                <div className="py-5">
-                    <div className="border-t border-gray-200" />
-                </div>
-            </div>
-
         </>
     );
 }
