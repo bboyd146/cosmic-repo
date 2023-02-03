fd274c3543563e23ad5f8a9e2e88d9f42a36ea28
bboyd146 <82745040+bboyd146@users.noreply.github.com>
2023-02-02 18:50:22 -0600

    updated mongo connection url and added package.json script

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,12 +2,10 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost/cosmic',
+    process.env.MONGODB_URI || 'mongodb://127.0.0.1/cosmic',
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-        // useCreateIndex: true,
-        // useFindAndModify: false
     }
 );
 
